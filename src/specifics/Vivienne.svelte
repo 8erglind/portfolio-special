f3412594e31ba810f1a665563f29a6002867944e
Berglind Brá Jóhannsdóttir <berglindbrajohannsdottir@Berglinds-MacBook-Pro.local>
2020-08-28 20:04:24 +0200

    viv pics missing from vid in

--- src/specifics/Vivienne.svelte
+++ src/specifics/Vivienne.svelte
@@ -5,18 +5,25 @@
 <div class="backgroundcolor">
 
 	<div style="
-	    position: absolute;
+	    position: relative;
 	    top: 20%;
 	    left: 2%;
 	    right: 2%;
 	    height: auto;
 	    padding-bottom: 2%;">
 	    <div style="padding:36.16% 0 0 0;position:relative;">
-	    	<iframe title="book" src="https://player.vimeo.com/video/331654391?autoplay=1&loop=1&title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+	    	<iframe title="book" src="https://player.vimeo.com/video/331654391?autoplay=1&loop=1&title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:96%;height:100%;" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
 	    </div>
 	    <script src="https://player.vimeo.com/api/player.js"></script>
 	</div>
-	<img class="img" alt="mynd" src="igms/viv/poster.jpg" />
+	<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+	<img class="img poster" alt="mynd" src="igms/viv/poster.jpg" /><br>
+	<br><br><br><br><br><br><br><br>
+	<img class="img" alt="mynd" src="igms/viv/22B.jpg" />
+	<img class="img" alt="mynd" src="igms/viv/38A.jpg" />
+	<img class="img" alt="mynd" src="igms/viv/41A.jpg" />
+	<img class="img" alt="mynd" src="igms/viv/42A.jpg" />
+	<img class="img" alt="mynd" src="igms/viv/44A.jpg" />
 	<br><br><br><br><br><br><br><br>
 
 </div>
@@ -33,9 +40,15 @@
 }
 img {
 	display: inline-block;
+	max-height: 80%;
+	max-width: 60%;
+	padding: 1%;
+	cursor: auto;
+}
+.poster {
 	max-height: 70%;
 	max-width: 60%;
-	margin-top: 60%;
+	/*margin-top: 60%;*/
 	padding: 1%;
 	cursor: auto;
 	border-radius: 15px;
